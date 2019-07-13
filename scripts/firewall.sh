067d1062423978ab266c84b66f5f689840861595
Bob Clarke <bobmclarke@gmail.com>
2019-07-13 21:30:54 +0100

    Adding template for home page

---
+++ scripts/firewall.sh
@@ -0,0 +1,2 @@
+/usr/libexec/ApplicationFirewall/socketfilterfw --remove /Users/clarkeb/go/src/github.com/gowiki/wiki && /usr/libexec/ApplicationFirewall/socketfilterfw --add /Users/clarkeb/go/src/github.com/gowiki/wiki && /usr/libexec/ApplicationFirewall/socketfilterfw --unblock /Users/clarkeb/go/src/github.com/gowiki/wiki
+
